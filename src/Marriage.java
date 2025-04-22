e94b4db23685e0ba9b07d5c34eed541cd4a48165
swagvillain <webmaster@cr1nge.com>
2025-04-21 21:18:10 -0700

    initial commit

---
+++ src/Marriage.java
@@ -0,0 +1,12 @@
+public class Marriage {
+    private Person person1;
+    private Person person2;
+
+    private int weddingYear;
+    private int weddingMonth;
+    private int weddingDay;
+
+    public Marriage(int year, int month, int day){
+        
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+    private int birthMonth;
+    private int birthDay;
+    private int deathYear;
+    private int deathMonth;
+    private int deathDay;
+
+    private Person mother;
+    private Person father;
+    private Person spouse;
+
+    private Marriage[] marriages;
+
+
+    /**
+     * @param fName Person's first name
+     * @param lName Person's last name
+     */
+    public Person(String fName, String lName){
+        this.firstName = fName;
+        this.lastName = lName;
+    }
+}
